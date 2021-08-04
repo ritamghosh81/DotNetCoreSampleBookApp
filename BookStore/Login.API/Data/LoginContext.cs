f5538f57176e33c4177201a9acfecedf688ab6ac
Ritam Ghosh <Ritam.Ghosh@hyland.com>
2021-08-04 19:02:20 +0530

    First commit

---
+++ BookStore/Login.API/Data/LoginContext.cs
@@ -0,0 +1,19 @@
+﻿using Login.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.API.Data
+{
+    public class LoginContext : IdentityDbContext<ApplicationUser>
+    {
+        public LoginContext(DbContextOptions<LoginContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ BookStore/Login.API/Repository/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Login.API.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Login.API.Repository
+{
+    public interface IAccountRepository
+    {
+        Task<IdentityResult> SignUpAsync(SignUpModel signUpModel);
+        Task<string> LoginAsync(SignInModel signInModel);
+    }
+}
